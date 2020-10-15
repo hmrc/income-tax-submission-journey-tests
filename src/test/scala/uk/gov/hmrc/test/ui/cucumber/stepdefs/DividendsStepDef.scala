f4813aec710654e4aa56c8a1ad587fa4a12bc22a
AdamSmithHMRC <72032501+AdamSmithHMRC@users.noreply.github.com>
2020-10-15 15:30:09 +0100

    SASS-34 Dividends Step Definition

--- src/test/scala/uk/gov/hmrc/test/ui/cucumber/stepdefs/DividendsStepDef.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/cucumber/stepdefs/DividendsStepDef.scala
@@ -1,41 +1,9 @@
-/*
- * Copyright 2020 HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package uk.gov.hmrc.test.ui.cucumber.stepdefs
 
 import uk.gov.hmrc.test.ui.pages.CommonPage
 import org.openqa.selenium.By
 
-class CommonStepDef extends BaseStepDef {
-
-  And ("""^clicks continue$"""){ () =>
-    driver.findElement(By.cssSelector("#main-content > div > div > form > button.govuk-button.govuk-\!-margin-right-1")).click()
-  }
-
-  When("""^the user clicks the provide updates button$""") { () =>
-    driver.findElement(By.cssSelector("#main-content > div > div > main > div > a")).click()
-  }
-
-  Then("""^the user will redirect to the Income Tax Submission start page$""") { () =>
-    driver.getCurrentUrl should be (CommonPage.url + "/start")
-  }
-
-  Then("""^the user should be on the overview page$""") { () =>
-    driver.getCurrentUrl should be (CommonPage.url + "/view")
-  }
+class DividendsStepDef extends BaseStepDef {
 
   Given ("""^the user clicks on the dividends link$^""") { () =>
     driver.findElement(By.cssSelector(
@@ -69,10 +37,6 @@ class CommonStepDef extends BaseStepDef {
 
   Then ("""^the user should be on the dividends check your answers page$""") { () =>
     driver.getCurrentUrl should be (CommonPage.url + "/dividends/summary")
-  }
-
-  When ("""^the user clicks the save and continue button$""") { () =>
-    driver.findElement(By.cssSelector("#main-content > div > div > form > button.govuk-button.govuk-\!-margin-right-1")).click()
   }
 
   When ("""^the user enters a value of £1000$""") { () =>

be85112a7692d8fe7b7c778fe3751de4316c6fd4
AdamSmithHMRC <72032501+AdamSmithHMRC@users.noreply.github.com>
2020-11-26 13:55:33 +0000

    SASS-154 edited interest feature

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,13 +3,13 @@ import sbt._
 object Dependencies {
 
   val test = Seq(
-    "uk.gov.hmrc"                %% "webdriver-factory"       % "0.12.0" % Test,
+    "uk.gov.hmrc"                %% "webdriver-factory"       % "0.15.0" % Test,
     "org.scalatest"              %% "scalatest"               % "3.1.4" % Test,
     "io.cucumber"                %% "cucumber-scala"          % "6.1.1" % Test,
     "io.cucumber"                %  "cucumber-junit"          % "6.1.1" % Test,
     "junit"                      %  "junit"                   % "4.12"  % Test,
     "com.novocode"               %  "junit-interface"         % "0.11"  % Test,
-    "uk.gov.hmrc"                %% "zap-automation"          % "2.7.0" % Test,
+    "uk.gov.hmrc"                %% "zap-automation"          % "2.8.0" % Test,
     "com.typesafe"               %  "config"                  % "1.3.2" % Test
   )
 

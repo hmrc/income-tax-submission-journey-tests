b3ea7c1106b03f1091e158457c875332a81f8a49
Robert Webb <28566263+robert-webb-hmrc@users.noreply.github.com>
2020-12-09 10:57:29 +0000

    zap-fix : some small script changes to fix zap build job

--- run_zap_tests.sh
+++ run_zap_tests.sh
@@ -17,7 +17,7 @@ fi
 # relevant pages of an application to ZAP. So tagging a subset of the journey tests or creating a
 # single ZAP focused journey test is sufficient.
 
-sh insert-data.sh
+sh insert_data.sh
 sbt -Dbrowser=$BROWSER -Denvironment=$ENV $DRIVER -Dzap.proxy=true "testOnly uk.gov.hmrc.test.ui.cucumber.runner.ZapRunner"
 sbt "testOnly uk.gov.hmrc.test.ui.ZapSpec"
 curl -X DELETE http://localhost:9303/setup/all-data

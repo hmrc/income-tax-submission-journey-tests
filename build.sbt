dcb550b57bde06b73b50a06da4e71faa0144d886
Joey Moore <22170468+joeyymoore@users.noreply.github.com>
2021-06-01 12:22:40 +0100

    SASS-829: Updating dependencies (#61)
    * updating dependencies
    
    * updated more dependencies

--- build.sbt
+++ build.sbt
@@ -1,19 +1,17 @@
 
 
 lazy val testSuite = (project in file("."))
-  .enablePlugins(SbtAutoBuildPlugin)
   .disablePlugins(JUnitXmlReportPlugin) //Required to prevent https://github.com/scalatest/scalatest/issues/1427
   .settings(
     name := "income-tax-submission-journey-tests",
     version := "0.1.0",
     scalaVersion := "2.12.12",
     scalacOptions ++= Seq("-feature"),
-    libraryDependencies ++= Dependencies.test,
       //The testOptions from SbtAutoBuildPlugin supports only ScalaTest. Resetting testOptions for Cucumber Tests.
       testOptions in Test := Seq.empty,
     resolvers ++= Seq(
-      Resolver.bintrayRepo("hmrc", "releases"),
-      Resolver.typesafeRepo("releases"),
-      "hmrc-releases" at "https://artefacts.tax.service.gov.uk/artifactory/hmrc-releases/"
-    )
+      MavenRepository("HMRC-open-artefacts-maven2", "https://open.artefacts.tax.service.gov.uk/maven2"),
+      Resolver.url("HMRC-open-artefacts-ivy2", url("https://open.artefacts.tax.service.gov.uk/ivy2"))(Resolver.ivyStylePatterns)
+    ),
+    libraryDependencies ++= Dependencies.test,
   )
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,13 +3,13 @@ import sbt._
 object Dependencies {
 
   val test = Seq(
-    "uk.gov.hmrc"                %% "webdriver-factory"       % "0.18.0" % Test,
-    "org.scalatest"              %% "scalatest"               % "3.2.0" % Test,
-    "io.cucumber"                %% "cucumber-scala"          % "6.1.1" % Test,
-    "io.cucumber"                %  "cucumber-junit"          % "6.1.1" % Test,
-    "junit"                      %  "junit"                   % "4.12"  % Test,
+    "uk.gov.hmrc"                %% "webdriver-factory"       % "0.20.0" % Test,
+    "org.scalatest"              %% "scalatest"               % "3.2.9" % Test,
+    "io.cucumber"                %% "cucumber-scala"          % "7.0.0" % Test,
+    "io.cucumber"                %  "cucumber-junit"          % "6.10.4" % Test,
+    "junit"                      %  "junit"                   % "4.13.2"  % Test,
     "com.novocode"               %  "junit-interface"         % "0.11"  % Test,
-    "uk.gov.hmrc"                %% "zap-automation"          % "2.9.0" % Test,
+    "uk.gov.hmrc"                %% "zap-automation"          % "2.10.0" % Test,
     "com.typesafe"               %  "config"                  % "1.4.1" % Test
   )
 

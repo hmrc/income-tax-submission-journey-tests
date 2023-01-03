2bd3aa9238de56ba4a63d7e8396cd0d10817a111
satyaayinala <satya.ayinala@digital.hmrc.gov.uk>
2023-01-03 13:43:37 +0000

    Updated Dependencies to latest versions

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,15 +2,15 @@ import sbt._
 
 object Dependencies {
 
-  val test = Seq(
-    "uk.gov.hmrc"                %% "webdriver-factory"       % "0.40.0" % Test,
-    "org.scalatest"              %% "scalatest"               % "3.2.9" % Test,
-    "io.cucumber"                %% "cucumber-scala"          % "7.0.0" % Test,
-    "com.typesafe.play"          %% "play-json"               % "2.6.13" % Test,
-    "io.cucumber"                %  "cucumber-junit"          % "6.10.4" % Test,
+  val test: Seq[ModuleID] = Seq(
+    "uk.gov.hmrc"                %% "webdriver-factory"       % "0.40.0"  % Test,
+    "org.scalatest"              %% "scalatest"               % "3.2.14"  % Test,
+    "io.cucumber"                %% "cucumber-scala"          % "8.13.1"  % Test,
+    "com.typesafe.play"          %% "play-json"               % "2.9.3"   % Test,
+    "io.cucumber"                %  "cucumber-junit"          % "7.10.1"  % Test,
     "junit"                      %  "junit"                   % "4.13.2"  % Test,
-    "com.novocode"               %  "junit-interface"         % "0.11"  % Test,
-    "com.typesafe"               %  "config"                  % "1.4.1" % Test
+    "com.novocode"               %  "junit-interface"         % "0.11"    % Test,
+    "com.typesafe"               %  "config"                  % "1.4.2"   % Test
   )
 
 }

28b37321c31fe0a041771168cadd6c3cb15fbe4e
Tos75 <tosin.olowoye@digital.hmrc.gov.uk>
2023-03-10 14:07:16 +0000

    Amend Browser Driver to fix Chrome 111 issue

--- src/test/scala/uk/gov/hmrc/test/ui/driver/BrowserDriver.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/driver/BrowserDriver.scala
@@ -18,10 +18,13 @@ package uk.gov.hmrc.test.ui.driver
 
 import com.typesafe.scalalogging.LazyLogging
 import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeOptions
 import uk.gov.hmrc.webdriver.SingletonDriver
 
 trait BrowserDriver extends LazyLogging {
   logger.info(s"Instantiating Browser: ${sys.props.getOrElse("browser", "'browser' System property not set. This is required")}")
+  val options = new ChromeOptions
+  options.addArguments("--remote-allow-origins=*")
 
-  implicit lazy val driver: WebDriver = SingletonDriver.getInstance()
+  implicit lazy val driver: WebDriver = SingletonDriver.getInstance(Some(options))
 }

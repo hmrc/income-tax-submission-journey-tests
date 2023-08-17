d78dec8530d2ef5f613feee2b156960108d86b57
Wale Soares <walesoares@gmail.com>
2023-08-17 09:09:58 +0100

    SASS-5532 add pensions runner

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ENV=${1:-local}
-BROWSER=${2:-firefox}
+BROWSER=${2:-chrome}
+SERVICE=${3:-all}
 # default to firefox as chrome's autofill hides bugs to do with incorrectly empty fields, see eg: SASS-4898 and SASS-4966
 DRIVER=
 
@@ -10,4 +11,10 @@ elif [ "$BROWSER" = "firefox" ]; then
     DRIVER="-Dwebdriver.gecko.driver=/usr/local/bin/geckodriver"
 fi
 
-sbt -Dbrowser=$BROWSER -Denvironment=$ENV $DRIVER "testOnly uk.gov.hmrc.test.ui.cucumber.runner.Runner"
+if [ "$SERVICE" = "pensions" ]; then
+  RUNNER="testOnly uk.gov.hmrc.test.ui.cucumber.runner.PensionsRunner"
+else
+  RUNNER="testOnly uk.gov.hmrc.test.ui.cucumber.runner.Runner"
+fi
+
+sbt -Dbrowser=$BROWSER -Denvironment=$ENV $DRIVER $RUNNER

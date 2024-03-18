4441c49485e02e7c6b1c6f33b2941f845d41e0a5
SamuelTwumBarima <93492001+SamuelTwumBarima@users.noreply.github.com>
2024-03-18 13:50:50 +0000

    update dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,17 +3,14 @@ import sbt._
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"                %% "ui-test-runner"          % "0.21.0"   % Test,
-    "org.scalatest"              %% "scalatest"               % "3.2.18"   % Test,
-    "io.cucumber"                %% "cucumber-scala"          % "8.20.0"   % Test,
-    "com.typesafe.play"          %% "play-json"               % "2.10.4"   % Test,
-    "io.cucumber"                %  "cucumber-junit"          % "7.15.0"   % Test,
-    "junit"                      %  "junit"                   % "4.13.2"   % Test,
-    "com.novocode"               %  "junit-interface"         % "0.11"     % Test,
-    "com.typesafe"               %  "config"                  % "1.4.2"    % Test,
-    "ch.qos.logback"             % "logback-classic"          % "1.5.1"    % Test,
-    "commons-io"                 %  "commons-io"              % "2.11.0"   % Test,
-    "com.vladsch.flexmark"       % "flexmark-all"             % "0.64.8"   % Test,
-    "org.scalatestplus"          %% "selenium-4-12"           % "3.2.17.0" % Test
+    "ch.qos.logback"       % "logback-classic" % "1.5.1"  % Test,
+    "com.vladsch.flexmark" % "flexmark-all"    % "0.64.8" % Test,
+    "org.scalatest"       %% "scalatest"       % "3.2.18" % Test,
+    "uk.gov.hmrc"         %% "ui-test-runner"  % "0.21.0" % Test,
+    "io.cucumber"         %% "cucumber-scala"  % "8.20.0" % Test,
+    "io.cucumber"          % "cucumber-junit"  % "7.15.0" % Test,
+    "junit"                % "junit"           % "4.13.2" % Test,
+    "com.novocode"         % "junit-interface" % "0.11"   % Test
   )
+
 }

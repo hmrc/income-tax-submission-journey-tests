e194b2beafdff8f82b710e8c20976d098521e7a0
SamuelTwumBarima <93492001+SamuelTwumBarima@users.noreply.github.com>
2024-09-06 14:47:20 +0100

    Add tests for dividends from unit trust and investments (#451)
    * Add tests for dividends from unit trust and investments
    
    * update tests

--- src/test/scala/uk/gov/hmrc/test/ui/pages/CommonElements.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/CommonElements.scala
@@ -84,7 +84,7 @@ trait CommonElements extends BasePage with Matchers {
     "Gilt-edged or accrued income securities task list"            -> By.className("GiltEdgedTitle"),
     "Cash dividends from UK stocks and shares1"           -> By.className("CashDividendsTitle"),
     "Stock dividends from UK companies task list"                  -> By.className("StockDividendsTitle"),
-    "Dividends from unit trusts or investment companies1" -> By.className("DividendsFromUnitTrustsTitle"),
+    "Dividends from unit trusts or investment companies task list" -> By.className("DividendsFromUnitTrustsTitle"),
     "Free or redeemable shares task list"                          -> By.className("FreeRedeemableSharesTitle"),
     "Close company loans written off or released1"        -> By.className("CloseCompanyLoansTitle"),
     "Life Insurance common task list"                     -> By.className("LifeInsuranceTitle"),
@@ -102,6 +102,7 @@ trait CommonElements extends BasePage with Matchers {
     "change stock dividends"                                     -> By.cssSelector("#main-content > div > div > dl > div:nth-child(4) > dd.govuk-summary-list__actions > a"),
     "change value of stock dividends"                            -> By.xpath("//*[contains(text(),'Value of stock dividends')]/parent::div/dd[2]/a"),
     "change value of free or redeemable shares"                  -> By.xpath("//*[contains(text(),' value of free or redeemable shares')]/parent::a"),
+    "change value of unit trusts or investment companies"        -> By.xpath("//*[contains(text(),' amount of dividends from unit trusts or investment companies')]/parent::a"),
     "change value of close company loan written off or released" -> By.xpath("//*[contains(text(),'Close company loan written off or released')]/parent::div/dd[2]"),
 
     //Interest Elements
